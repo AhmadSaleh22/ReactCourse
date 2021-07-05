334368913b3a9c36bfbeb11679f5d8c9fcb8c575
Ahmad Saleh <dev.ahmadsaleh@gmail.com>
2021-07-06 02:36:39 +0300

    Adding functions for on change and on click for inputs and buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Name from './Name';
+import { useState } from 'react';
+
 
 function App() {
+  
+  
+  const [count, setCount] = useState(0);
+  
+  const decrement  =()=> {
+    setCount(count - 1);
+  }
+  
+  const onChangeFun = (event ) =>{
+    setTimeout(function (){
+      console.log(event.target.value);
+    }, 3000);
+  }
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Name name="ahmad saleh" />
+          <div class="list-group">
+            <button type="button" class="list-group-item list-group-item-action active" onClick={decrement}>Active item</button>
+            <div class="form-group">
+              <label for="username">User Name</label>
+              <input type="text" onChange= {onChangeFun}
+                class="form-control" name="username" id="username" aria-describedby="helpId" placeholder=""/>
+              <small id="helpId" class="form-text text-muted">Help text</small>
+            </div>
+          </div>
+          {count}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
